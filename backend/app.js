7486bf4aca8631ca6458a6a41c5f08f147ff0314
sujeetRawat <sujeetrawat890@gmail.com>
2025-02-18 13:49:27 +0530

    Added Basic boilerplate enviornment

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express= require("express");
+const app =express();
+
+app.use(express.json());
+
+//Routes import
+
+const product = require('./routes/productroute');
+
+app.use("/api/v1",product);
+
+module.exports = app;
---
+++ backend/routes/productroute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getAllProducts } = require("../controllers/productController");
+
+const router = express.Router();
+
+router.get("/products",getAllProducts)
+
+
+
+
+module.exports = router;
